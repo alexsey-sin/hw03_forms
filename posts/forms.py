f2fdd0445936f210b39748c7898de432725b9a86
alexsey-sin <alexey-sin@yandex.ru>
2021-03-11 21:40:51 +0300

    finish

---
+++ posts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('group', 'text')
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+
+        if len(text) == 0:
+            raise forms.ValidationError(
+                'Это поле не должно быть пустым!', params={'text': text},
+            )
+
+        return text
